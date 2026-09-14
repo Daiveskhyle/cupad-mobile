c4696bb534b8502a5cd92d0c3bdfe9f2786d01f7
Daiveskhyle <73757950+Daiveskhyle@users.noreply.github.com>
2026-09-14 11:54:01 +0100

    fix(web): resolve expo-sqlite to web shim

---
+++ metro.config.js
@@ -0,0 +1,22 @@
+const { getDefaultConfig } = require('expo/metro-config');
+
+const config = getDefaultConfig(__dirname);
+
+const defaultResolveRequest = config.resolver.resolveRequest;
+
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  if (platform === 'web' && moduleName === 'expo-sqlite') {
+    return {
+      filePath: require.resolve('./src/shims/expo-sqlite.web.ts'),
+      type: 'sourceFile',
+    };
+  }
+
+  if (defaultResolveRequest) {
+    return defaultResolveRequest(context, moduleName, platform);
+  }
+
+  return context.resolveRequest(context, moduleName, platform);
+};
+
+module.exports = config;
