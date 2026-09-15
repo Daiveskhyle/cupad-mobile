567d0fb249eef4e2a797857f32133e03f46156fd
Daiveskhyle <73757950+Daiveskhyle@users.noreply.github.com>
2026-09-15 15:58:26 +0100

    Hide root header on welcome screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout() {
           contentStyle: { backgroundColor: colors.background },
         }}
       >
+        <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="co" options={{ headerShown: false }} />
